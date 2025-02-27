f90a2b1797bda2859d918a67f40c2ff66a421a0c
Emily <emilys@genstudents.org>
2025-02-27 11:00:10 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+import Home from "./pages/home/Home"
 
 
 function App() {
 
   return (
-    
+    <>
+      <ToastContainer />
+
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+  
+        </Route>
+      </Routes>
+    </>
   )
 }
 
