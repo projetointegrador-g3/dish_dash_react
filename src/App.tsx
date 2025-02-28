144ce6ea8e17f2b114677ade9b586f0ede92be6f
Emily <emilys@genstudents.org>
2025-02-28 07:44:10 -0300

    :broom: Limpeza de Código

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/home/Home";
 import Categorias from "./components/categorias/Categorias";
 import Navbar from "./components/navbar/Navbar";
-import NavbarInicio from "./components/navbarinicio/NavbarInicio";
+import NavbarInicio from "./components/navbarInicial/NavbarInicio";
 import Login from "./pages/login/Login";
 import Sobre from "./pages/sobre/Sobre";
 import Sidebar from "./components/sidebar/Sidebar";
@@ -11,23 +11,27 @@ import Inicio from "./pages/inicio/Inicio";
 import Perfil from "./components/perfil/Perfil";
 import Configuracao from "./components/settings/Configuracao";
 
-function App() {
+export default function App() {
   const location = useLocation(); // Obtém a localização atual do navegador.
   const hideNavbarAndSidebar = ["/login", "/", "/sobre"].includes(location.pathname); // Verifica se a Navbar e Sidebar devem ser ocultadas.
   const noPaddingPages = ["/", "/login", "/sobre"]; // Define páginas que não devem ter padding.
 
   return (
     <>
-      <ToastContainer /> {/* Componente para exibir notificações. */}
+      <ToastContainer /> 
+
       <div>
         <div className="flex h-screen overflow-hidden">
           {!hideNavbarAndSidebar && <Sidebar />} {/* Renderiza o Sidebar, se necessário. */}
 
           <div className={`flex flex-col flex-1 ${!hideNavbarAndSidebar ? 'ml-20' : ''}`}>
+
             {/* Renderiza o Navbar, exceto nas páginas de login, inicial e sobre */}
             {location.pathname !== "/login" && location.pathname !== "/" && location.pathname !== "/sobre" && <Navbar />}
+            
             {/* Renderiza o NavbarInicio nas páginas inicial e sobre */}
             {(location.pathname === "/" || location.pathname === "/sobre") && <NavbarInicio />}
+            
             <div className={`flex-1 overflow-auto ${noPaddingPages.includes(location.pathname) ? 'p-0' : 'p-6'}`}>
               <Routes>
                 <Route path="/" element={<Inicio />} />
@@ -37,7 +41,7 @@ function App() {
                 <Route path="/categorias" element={<Categorias />} />
                 <Route path="/perfil" element={<Perfil />} />
                 <Route path="/configuracoes" element={<Configuracao />} />
-              </Routes> {/* Define as rotas para os componentes. */}
+              </Routes> 
             </div>
           </div>
         </div>
@@ -46,14 +50,5 @@ function App() {
   );
 }
 
-function AppWrapper() {
-  return (
-    <BrowserRouter>
-      <App /> {/* Envolve o App dentro de BrowserRouter para habilitar a navegação por rotas. */}
-    </BrowserRouter>
-  );
-}
-
-export default AppWrapper; // Exporta o componente AppWrapper como padrão.
 
 
